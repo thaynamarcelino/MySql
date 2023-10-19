3e406cf6c53250f6946e71214994e814da6ecad9
Thayná Cristina <134014880+thaynamarcelino@users.noreply.github.com>
2023-10-19 10:39:51 -0300

    fiz algumas atualizações

--- Aula03/criando um banco de dados.sql
+++ Aula03/criando um banco de dados.sql
@@ -1,14 +1,1 @@
 CREATE DATABASE cadastro;
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
-
-CREATE TABLE pessoas (
-id INT NOT NULL auto_increment,
-nome VARCHAR(30),
-nascimento DATE,
-sexo ENUM('M', 'F'),
-peso DECIMAL(5,2),
-altura DECIMAL(3,2),
-nacionalidade VARCHAR(20) DEFAULT 'Brasil'
-PRIMARY KEY (id)
-) DEFAULT CHARSET = utf8;
--- Aula04/Melhorando banco de dados.sql
+++ Aula04/Melhorando banco de dados.sql
@@ -1,11 +1,14 @@
-create table pessoas(
-nome varchar(30),
-idade tinyint(3),
-sexo char(1),
-altura float,
-nacionalidade varchar(20)
-);
+CREATE DATABASE cadastro;
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
 
-
-create database cadastro;
-
+CREATE TABLE pessoas (
+id INT NOT NULL auto_increment,
+nome VARCHAR(30),
+nascimento DATE,
+sexo ENUM('M', 'F'),
+peso DECIMAL(5,2),
+altura DECIMAL(3,2),
+nacionalidade VARCHAR(20) DEFAULT 'Brasil'
+PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8;
