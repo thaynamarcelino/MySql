611ed6aa60473ff04655aedbb9eb20704b11c85f
Thayná Cristina <134014880+thaynamarcelino@users.noreply.github.com>
2023-11-16 21:10:31 -0300

    atualizei a aula 05

---
+++ aula 05 - Inserindo dados na tabela (insert into).sql
@@ -0,0 +1,12 @@
+insert into pessoas
+(id, nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+(DEFAULT, 'Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
+(DEFAULT, 'Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal'),
+(DEFAULT, 'Creuza', '1920-12-30', 'F', '50.2', '1.65', DEFAULT),
+(DEFAULT, 'Adalgiza', '1930-11-2', 'F', '63.2', '1.75', 'Irlanda'),
+(DEFAULT, 'Cláudio', '1975-4-22', 'M', '99.0', '2.15', 'Brasil'),
+(DEFAULT, 'Pedro', '1999-12-3', 'M', '87', '2', default),
+(DEFAULT, 'Janaina', '1987-11-12', 'F', '75.4', '1.66', 'EUA');
+
+select * from pessoas;
