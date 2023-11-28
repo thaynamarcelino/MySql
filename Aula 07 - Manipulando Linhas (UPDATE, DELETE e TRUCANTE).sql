edb835717306a8560cf8f0b73a91b9ac726f7093
Thayná Cristina <134014880+thaynamarcelino@users.noreply.github.com>
2023-11-28 18:28:24 -0300

    Create Aula 07 - Manipulando Linhas (UPDATE, DELETE e TRUCANTE).sql

---
+++ Aula 07 - Manipulando Linhas (UPDATE, DELETE e TRUCANTE).sql
@@ -0,0 +1,39 @@
+use cadastro;
+
+select * from gafanhotos;
+select * from cursos;
+
+insert into cursos values
+('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Programação', '20', '15', '2014'),
+('3','Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('4', 'PGP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+('5', 'Jarva', 'Introdução á Linguagem Java', '10', '29', '2000'),
+('6', 'MySQL', 'Bancos de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Rítmicas', '40', '30', '2018'),
+('9', 'Cozinha Árabe', 'Aprenda a fazer Kibe', '40', '30', '2018'),
+('10', 'YouTuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
+
+
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
+
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
+
+update cursos
+set nome = 'Java', ano = '2015', carga = '40'
+where idcurso = '5'
+limit 1;
+
+delete from cursos
+where idcurso = '8';
+
+delete from cursos
+where ano = '2050'
+limit 2;
+
+truncate table cursos;
